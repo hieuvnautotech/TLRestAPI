9c30177780de0882e2c3edd4729cf54d1948a890
hieu <hieuvnautotech@gmail.com>
2022-07-20 12:45:21 +0700

    lam o cty 20-7

---
+++ TLRestAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TLRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+    }
+}
